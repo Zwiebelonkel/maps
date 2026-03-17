8ec853325a1034acfe01ba26e19f1e5dab7d3d87
Luca <lucamuller2004@gmail.com>
2026-03-17 21:11:34 +0100

    settings.component.ts aktualisieren

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { SettingsService } from '../../../services/settings.service';
+import { SettingsService } from '../../services/settings.service';
 
 @Component({
   selector: 'app-settings',
@@ -12,22 +12,24 @@ import { SettingsService } from '../../../services/settings.service';
 })
 export class SettingsComponent {
   isOpen = false;
+  @Output() gameReset = new EventEmitter<void>();
 
-  get maxRenderTiles() {
-    return this.settings.snapshot.maxRenderTiles;
-  }
+  get maxRenderTiles() { return this.settings.snapshot.maxRenderTiles; }
 
   constructor(public settings: SettingsService) {}
 
-  toggle() {
-    this.isOpen = !this.isOpen;
-  }
+  toggle() { this.isOpen = !this.isOpen; }
 
   updateMaxRenderTiles(val: number) {
     this.settings.update({ maxRenderTiles: val });
   }
 
-  reset() {
-    this.settings.reset();
+  reset() { this.settings.reset(); }
+
+  resetGame() {
+    if (confirm('Möchtest du wirklich deinen gesamten Fortschritt löschen?')) {
+      this.gameReset.emit();
+      this.isOpen = false;
+    }
   }
 }
