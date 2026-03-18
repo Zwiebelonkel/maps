d72dac27dbc5e5478e5f095364094a2421b6ed48
Luca <lucamuller2004@gmail.com>
2026-03-18 22:45:28 +0100

    session-summary.component.ts aktualisieren

--- src/app/components/session-summary/session-summary.component.ts
+++ src/app/components/session-summary/session-summary.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SessionStats, SessionService } from '../../services/session.service';
+import { SessionStats, SessionService } from '../../../services/session.service';
 
 @Component({
   selector: 'app-session-summary',
