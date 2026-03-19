92bf377505dd3093dac096b62df0c8f152bef19e
Luca <lucamuller2004@gmail.com>
2026-03-19 21:20:59 +0100

    marker-list.component.ts erstellen

---
+++ src/app/components/marker-list/marker-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MarkerService } from '../../services/marker.service';
+import { UserMarker } from '../../models/user-marker.model';
+
+@Component({
+  selector: 'app-marker-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './marker-list.component.html',
+  styleUrls: ['./marker-list.component.scss'],
+})
+export class MarkerListComponent {
+  constructor(public markerService: MarkerService) {}
+
+  delete(marker: UserMarker) {
+    this.markerService.removeMarker(marker.id);
+  }
+}
