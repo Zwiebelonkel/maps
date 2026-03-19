d018631e739507865e3a265a9b5d8e3fbaf4f110
Luca <lucamuller2004@gmail.com>
2026-03-19 22:42:53 +0100

    burger-menu.component.ts aktualisieren

--- src/app/components/burger-menu/burger-menu.component.ts
+++ src/app/components/burger-menu/burger-menu.component.ts
@@ -14,18 +14,61 @@ export class BurgerMenuComponent {
   @Output() openSettings = new EventEmitter<void>();
   @Output() openMarkers = new EventEmitter<void>();
 
+  isOpen = true;
+  isClosing = false;
+
+  private touchStartY = 0;
+  private touchCurrentY = 0;
+  private isDraggingHeader = false;
+
+  closeWithAnimation() {
+    if (this.isClosing) return;
+
+    this.isClosing = true;
+
+    setTimeout(() => {
+      this.isOpen = false;
+      this.isClosing = false;
+      this.close.emit();
+    }, 320);
+  }
+
   onSession() {
     this.toggleSession.emit();
-    this.close.emit();
+    this.closeWithAnimation();
   }
 
   onSettings() {
     this.openSettings.emit();
-    this.close.emit();
+    this.closeWithAnimation();
   }
 
   onMarkers() {
     this.openMarkers.emit();
-    this.close.emit();
+    this.closeWithAnimation();
+  }
+
+  onHeaderTouchStart(e: TouchEvent) {
+    this.touchStartY = e.touches[0].clientY;
+    this.touchCurrentY = this.touchStartY;
+    this.isDraggingHeader = true;
+  }
+
+  onHeaderTouchMove(e: TouchEvent) {
+    if (!this.isDraggingHeader) return;
+    this.touchCurrentY = e.touches[0].clientY;
+  }
+
+  onHeaderTouchEnd() {
+    if (!this.isDraggingHeader) return;
+
+    const diff = this.touchCurrentY - this.touchStartY;
+    if (diff > 80) {
+      this.closeWithAnimation();
+    }
+
+    this.touchStartY = 0;
+    this.touchCurrentY = 0;
+    this.isDraggingHeader = false;
   }
 }
