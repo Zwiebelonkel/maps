b6e7b78f6d9c17e90c749012b6819a456da0dbd6
Luca <lucamuller2004@gmail.com>
2026-03-19 20:42:02 +0100

    user-marker.model.ts erstellen

---
+++ models/user-marker.model.ts
@@ -0,0 +1,10 @@
+// src/app/models/user-marker.model.ts
+
+export interface UserMarker {
+  id: string;
+  lat: number;
+  lng: number;
+  name: string;
+  description: string;
+  createdAt: number;
+}
