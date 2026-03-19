d39583e300e8abd0d54e194e2f08571a239cd5ba
Luca <lucamuller2004@gmail.com>
2026-03-19 21:54:28 +0100

    marker-list.component.ts aktualisieren

--- src/app/components/marker-list/marker-list.component.ts
+++ src/app/components/marker-list/marker-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MarkerService } from '../../services/marker.service';
+import { MarkerService } from '../../../services/marker.service';
 import { UserMarker } from '../../../../models/user-marker.model';
 
 @Component({
