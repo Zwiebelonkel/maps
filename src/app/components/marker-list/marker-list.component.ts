97509d7bf0079990eb651c78b758aa0a701d5503
Luca <lucamuller2004@gmail.com>
2026-03-19 21:51:10 +0100

    marker-list.component.ts aktualisieren

--- src/app/components/marker-list/marker-list.component.ts
+++ src/app/components/marker-list/marker-list.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MarkerService } from '../../../services/marker.service';
-import { UserMarker } from '../../../../models/user-marker.model';
+import { MarkerService } from '../../services/marker.service';
+import { UserMarker } from '../../models/user-marker.model';
 
 @Component({
   selector: 'app-marker-list',
@@ -18,7 +18,4 @@ export class MarkerListComponent {
   delete(marker: UserMarker) {
     this.markerService.removeMarker(marker.id);
   }
-
-  closeList() {
-    this.close.emit();
-  }
+}
