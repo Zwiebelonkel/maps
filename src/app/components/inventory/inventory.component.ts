4c259f38c21f010a1a8ed1bce31535039547d60d
Luca <lucamuller2004@gmail.com>
2026-03-29 01:10:48 +0100

    inventory.component.ts aktualisieren

--- src/app/components/inventory/inventory.component.ts
+++ src/app/components/inventory/inventory.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InventoryService } from '../../../services/inventory.service';
 import { ShopItem } from '../../config/game.config';
@@ -14,11 +14,36 @@ export class InventoryComponent {
   @Output() close = new EventEmitter<void>();
   @Output() activateBomb = new EventEmitter<ShopItem>();
 
+  isOpen = true;
+  isClosing = false;
+
+  private touchStartY = 0;
+  private touchCurrentY = 0;
+
   constructor(public inventoryService: InventoryService) {}
+
+  closeWithAnimation() {
+    this.isClosing = true;
+    setTimeout(() => this.close.emit(), 320);
+  }
 
   onActivate(item: ShopItem, index: number) {
     this.inventoryService.remove(index);
     this.activateBomb.emit(item);
-    this.close.emit();
+    this.closeWithAnimation();
+  }
+
+  onHeaderTouchStart(e: TouchEvent) {
+    this.touchStartY = e.touches[0].clientY;
+  }
+
+  onHeaderTouchMove(e: TouchEvent) {
+    this.touchCurrentY = e.touches[0].clientY;
+  }
+
+  onHeaderTouchEnd() {
+    if (this.touchCurrentY - this.touchStartY > 60) {
+      this.closeWithAnimation();
+    }
   }
 }
