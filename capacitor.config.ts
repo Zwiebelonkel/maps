d37bd559b464aabcabed0e8277708bd9df18b6e2
Zwiebelonkel <lucamuller2004@gmail.com>
2026-03-30 20:07:41 +0200

    andoird init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.maps.app',
+  appName: 'map-explorer',
+  webDir: 'docs',
+};
+
+export default config;
