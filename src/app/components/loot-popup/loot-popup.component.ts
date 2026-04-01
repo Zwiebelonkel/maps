071ba9f42ca8365b405f94a00193037e1cd0874b
Luca <lucamuller2004@gmail.com>
2026-04-01 22:29:24 +0200

    loot-popup.component.ts aktualisieren

--- src/app/components/loot-popup/loot-popup.component.ts
+++ src/app/components/loot-popup/loot-popup.component.ts
@@ -12,12 +12,24 @@ import { LootResult } from '../../../services/loot.service';
 export class LootPopupComponent {
   loot: LootResult | null = null;
   visible = false;
+  isClosing = false;
+
   private hideTimeout: any;
 
   show(loot: LootResult) {
     clearTimeout(this.hideTimeout);
+
     this.loot = loot;
     this.visible = true;
-    this.hideTimeout = setTimeout(() => (this.visible = false), 2500);
+    this.isClosing = false;
+
+    this.hideTimeout = setTimeout(() => {
+      this.isClosing = true;
+
+      setTimeout(() => {
+        this.visible = false;
+        this.isClosing = false;
+      }, 180); // Dauer der Exit Animation
+    }, 2300);
   }
 }
