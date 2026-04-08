763688ae15cea33359bb8f7fa7a0833ff10bacf6
Luca <lucamuller2004@gmail.com>
2026-04-08 12:37:49 +0200

    ascension-panel.component.ts erstellen

---
+++ src/app/components/ascension-panel/ascension-panel.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AscensionService } from '../../services/ascension.service';
+
+@Component({
+  selector: 'app-ascension-panel',
+  templateUrl: './ascension-panel.component.html',
+  styleUrls: ['./ascension-panel.component.scss']
+})
+export class AscensionPanelComponent {
+  isOpen = false;
+
+  constructor(public ascension: AscensionService) {}
+
+  open() {
+    this.isOpen = true;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
+
+  get progressPercent(): number {
+    return (this.ascension.points / this.ascension.getPointsNeeded()) * 100;
+  }
+}
